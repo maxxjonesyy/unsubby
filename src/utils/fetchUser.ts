43983c2da3ff5148a21e799d58968b4aef7492db
Max Jones <maxxjonesyy@gmail.com>
2023-11-27 22:50:06 +1100

    simplify fetch user

--- src/utils/fetchUser.ts
+++ src/utils/fetchUser.ts
@@ -1,33 +1,32 @@
 import axios from "axios";
-import endpoints from '../data/endpoints.json'
+import endpoints from "../data/endpoints.json";
 
+export default async function fetchUser(token: string) {
+  if (!token) {
+    console.error("No token found");
+    return;
+  }
 
-export default async function fetchUser(token : string) {
-    const headers = {
-      Authorization: `Bearer ${token}`,
-    };
+  const headers = {
+    Authorization: `Bearer ${token}`,
+  };
 
-    try {
-        if(!token) {
-            throw new Error('No token found');
-        } else {
-            const response = await axios.get(endpoints.userInfo, {headers})
-            if(response.status === 200) {
-                const { data } = response
+  try {
+    const response = await axios.get(endpoints.userInfo, { headers });
 
-                if(data) {
-                    const userData = {
-                        id: data.sub,
-                        name: data.name,
-                        image: data.picture
-                    }
+    if (response.status === 200 && response.data) {
+      const { sub, name, picture } = response.data;
+      const userData = {
+        id: sub,
+        name,
+        image: picture,
+      };
 
-                    return userData;
-                }
-            }
-        }
-    } catch(error) {
-        console.error(error);
+      return userData;
     }
-}
+  } catch (error) {
+    console.error("Error fetching user:", error);
+  }
 
+  return;
+}
