624dd5d4bcbecb3754d49414d104fb0b2223e3ef
Max Jones <maxxjonesyy@gmail.com>
2023-12-05 19:58:18 +1100

    clean up app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,17 @@
 import { useEffect, useState } from "react";
 
-import handleAuth from "./services/google-authentication/handleAuth";
 import setAuthToken from "./services/google-authentication/setAuthToken";
 import fetchUser from "./utils/fetchUser";
-import fetchMessages from "./utils/fetchMessages";
+import Login from "./views/Login";
 
 function App() {
   const [user, setUser] = useState<Record<string, string> | undefined>();
-  const [loading, setLoading] = useState(false);
 
   const authHasRun: string = sessionStorage.getItem("authHasRun") || "";
   const token: string = sessionStorage.getItem("token") || "";
 
-  // Setup homepage UI and theming (Add info text on how to use and features)
-
   useEffect(() => {
     async function setup() {
-      setLoading(true);
       try {
         if (authHasRun && !token) {
           setAuthToken();
@@ -26,8 +21,6 @@ function App() {
         }
       } catch (error) {
         throw new Error(`${error}`);
-      } finally {
-        setLoading(false);
       }
     }
     setup();
@@ -35,8 +28,7 @@ function App() {
 
   return (
     <>
-      <p>Unsubby</p>
-      <button onClick={handleAuth}>Test Fetch</button>
+      <Login />
     </>
   );
 }
