d0220c983dbc2bb30fab4fc987245ada28d28f73
Max Jones <maxxjonesyy@gmail.com>
2023-12-06 20:28:01 +1100

    Create Login component

---
+++ src/views/Login.tsx
@@ -0,0 +1,19 @@
+import handleAuth from "../services/google-authentication/handleAuth";
+import Button from "../components/Button";
+import google from "../assets/icons/google.svg";
+
+function Login() {
+  return (
+    <div className='h-screen w-full flex flex-col items-center justify-center gap-5'>
+      <h1 className='font-black text-6xl bg-gradient-to-r from-violet-600 via-blue-600 to-violet-600 animate-text rounded-lg text-transparent shadow-lg bg-clip-text'>
+        Unsubby
+      </h1>
+      <h2 className='text-lg text-center shadow-md'>
+        A simple solution to managing your Gmail subscriptions.
+      </h2>
+      <Button onClick={handleAuth} text={"Login with Google"} icon={google} />
+    </div>
+  );
+}
+
+export default Login;
