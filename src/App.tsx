6218536554a2ed1936e9a1846135b08482d8e021
Max Jones <maxxjonesyy@gmail.com>
2023-12-06 21:42:02 +1100

    Fix variable scope for user setup function

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,40 @@
 import { useEffect, useState } from "react";
 
+import Login from "./views/Login";
+import Home from "./views/Home";
+
 import setAuthToken from "./services/google-authentication/setAuthToken";
 import fetchUser from "./utils/fetchUser";
-import Login from "./views/Login";
 
 function App() {
   const [user, setUser] = useState<Record<string, string> | undefined>();
 
-  const authHasRun: string = sessionStorage.getItem("authHasRun") || "";
-  const token: string = sessionStorage.getItem("token") || "";
-
   useEffect(() => {
+    const authHasRun: string | undefined =
+      sessionStorage.getItem("authHasRun") ?? undefined;
+
+    const token: string | undefined =
+      sessionStorage.getItem("token") ?? undefined;
+
     async function setup() {
       try {
         if (authHasRun && !token) {
           setAuthToken();
         } else if (authHasRun && token && !user) {
           const userData = await fetchUser(token);
-          setUser(userData);
+          if (userData) {
+            setUser(userData);
+          }
         }
       } catch (error) {
-        throw new Error(`${error}`);
+        console.error("Error during setup:", error);
       }
     }
+
     setup();
   }, []);
 
-  return (
-    <>
-      <Login />
-    </>
-  );
+  return <>{user ? <Home user={user} /> : <Login />}</>;
 }
 
 export default App;
