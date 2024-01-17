a4db06be84b98ee3cda9a3a0629567a6c55a0269
Max Jones <maxxjonesyy@gmail.com>
2024-01-17 22:28:42 +1100

    update: add supabase functions [getSubscriptions, addSubscriptions]

--- src/services/supabase/supabase.ts
+++ src/services/supabase/supabase.ts
@@ -1,4 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
+import renderAlert from "../../utils/renderAlert";
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
@@ -18,7 +19,49 @@ async function handleLogin() {
 }
 
 async function handleLogout() {
-  await supabase.auth.signOut({ scope: "global" });
+  supabase.auth.signOut({ scope: "global" });
 }
 
-export { supabase, handleLogin, handleLogout };
+async function getSubscriptions() {
+  const { data: subscriptions } = await supabase
+    .from("profiles")
+    .select("unsubscribed");
+
+  if (subscriptions) {
+    return subscriptions[0].unsubscribed;
+  } else return [];
+}
+
+async function addSubscriptions(user: any, emailsArray: string[]) {
+  if (emailsArray.length > 0) {
+    const pathParams = {
+      id: user.id,
+      name: user.user_metadata.name,
+      email: user.email,
+    };
+
+    const { data } = await supabase.from("profiles").select("unsubscribed");
+
+    if (data) {
+      const { unsubscribed } = data[0];
+
+      await supabase.from("profiles").upsert([
+        {
+          ...pathParams,
+          unsubscribed:
+            unsubscribed !== null
+              ? [...unsubscribed, ...emailsArray]
+              : [...emailsArray],
+        },
+      ]);
+    }
+  }
+}
+
+export {
+  supabase,
+  handleLogin,
+  handleLogout,
+  getSubscriptions,
+  addSubscriptions,
+};
