bc5afc2e7ab6adfc6fbceb055c276a41d950a64f
Max Jones <maxxjonesyy@gmail.com>
2024-01-21 13:34:34 +1100

    add workflow of app to faq

--- src/data/faq.ts
+++ src/data/faq.ts
@@ -1,5 +1,10 @@
 const faq = [
   {
+    question: "How do I use this?",
+    answer:
+      "Log in, click the links to unsubscribe. Then select and delete those you've unsubscribed from, and we will remove those from your inbox for you!",
+  },
+  {
     question: "How does Unsubby work?",
     answer:
       "Simply login with with your Google account (an authentication service provided by Google themselves) and you are able to fetch your current subscriptions. The unsubscribe links which are usually hidden, or a pain to find are now listed, you can click on these and swiftly unsubscribe from whichever ones you wish.",
