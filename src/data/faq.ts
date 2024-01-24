ec3a640a78228a40bc73521915fb02c5c09a6e27
Maxwell Jones <maxxjonesyy@gmail.com>
2024-01-25 10:24:19 +1100

    update faq

--- src/data/faq.ts
+++ src/data/faq.ts
@@ -2,27 +2,27 @@ const faq = [
   {
     question: "How do I use this?",
     answer:
-      "Log in, click the links to unsubscribe. Then select and delete those you've unsubscribed from, and we will remove those from your inbox for you!",
+      "Log in, click the provided links to unsubscribe. Afterward, select and delete the ones you've unsubscribed from, and we'll take care of removing those from your inbox.",
   },
   {
     question: "How does Unsubby work?",
     answer:
-      "Simply login with with your Google account (an authentication service provided by Google themselves) and you are able to fetch your current subscriptions. The unsubscribe links which are usually hidden, or a pain to find are now listed, you can click on these and swiftly unsubscribe from whichever ones you wish.",
+      "Simply log in with your Google account, utilizing the authentication service provided by Google. Once logged in, you can fetch your current subscriptions. The usually hidden or hard-to-find unsubscribe links are now conveniently listed, allowing you to swiftly unsubscribe from the ones you choose.",
   },
   {
     question: "Do we store your data?",
     answer:
-      "We don't store any personal data in our database. We only store your list of subscriptions, everything else you can find within your sessionStorage (browser) which is deleted each time you close your tab.",
+      "No personal data is stored in our database. We only retain your list of subscriptions. Everything else, including your information, is kept within your browser's sessionStorage and is deleted each time you close your tab.",
   },
   {
     question: "Why am I getting an error?",
     answer:
-      "Access tokens used to fetch your emails are very short lived. If you are getting an error there is high change it has expired and you will just need to log in again.",
+      "Access tokens used to fetch your emails have a short lifespan. If you encounter an error, there's a high chance it has expired, and you'll need to log in again.",
   },
   {
     question: "Can I use another email provider?",
     answer:
-      "At the current moment in time we only support Gmail, this may change further in the future.",
+      "Currently, we exclusively support Gmail. However, this may change in the future as we continue to improve and expand our service.",
   },
 ];
 
